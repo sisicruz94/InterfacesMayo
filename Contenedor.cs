bf30ad597ffa6bcb6e15787566137764282c3b68
sisicruz94 <148384964+sisicruz94@users.noreply.github.com>
2024-05-27 11:43:59 +0200

    Add files via upload

---
+++ Contenedor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen5BPJ
+{
+    /**
+         * Inicializa un contenedor con todos sus datos
+         * @param referencia
+         * @param peso
+         * @param volumen
+         * @param tipoContenido 
+         */
+
+
+    /// <summary>
+    /// Clase para registro de contenedores para examen de Desarrollo de Interfaces Distancia
+    /// </summary>
+    public class Contenedor
+    {
+
+
+
+        /// <summary>
+        /// Inicializa un contenedor con todos sus datos
+        /// </summary>
+        /// <param name="referencia"></param>
+        /// <param name="peso"></param>
+        /// <param name="volumen"></param>
+        /// <param name="tipoContenido"></param>        
+        public Contenedor(String referencia, int peso, int volumen, String tipoContenido)
+        {
+            this.Referencia = referencia;
+            this.Peso = peso;
+            this.Volumen = volumen;
+            this.TipoContenido = tipoContenido;
+        }
+
+        /// <summary>
+        /// Referencia del contenedor
+        /// </summary>
+        public string Referencia { get; set; }
+
+        /// <summary>
+        /// Peso del contenedor
+        /// </summary>
+        public int Peso { get; set; }
+
+        /// <summary>
+        /// Volumen del contenedor
+        /// </summary>
+        public int Volumen { get; set; }
+
+        /// <summary>
+        /// Tipo de contenido
+        /// </summary>
+        public string TipoContenido { get; set; }
+    }
+}
---
+++ LogicaNegocio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen5BPJ
+{
+    public class LogicaNegocio
+    {
+
+        /// <summary>
+        /// Obtiene una lista completa de contenedores
+        /// </summary>
+        /// <returns>Devuelve una lista con todos los contenedores</returns>
+        public static ObservableCollection<Contenedor> GetAllContenedores()
+        {
+            ObservableCollection<Contenedor> retValue = new ObservableCollection<Contenedor>();
+            retValue.Add(new Contenedor("D0976", 72, 53, "IND"));
+            retValue.Add(new Contenedor("X9769", 52, 67, "IND"));
+            retValue.Add(new Contenedor("P0966", 74, 75, "FRA"));
+            retValue.Add(new Contenedor("D0980", 51, 15, "LIQ"));
+            retValue.Add(new Contenedor("F76435", 56, 27, "SOL"));
+            retValue.Add(new Contenedor("L7272", 77, 35, "FRA"));
+            retValue.Add(new Contenedor("K0962", 153, 46, "LIQ"));
+            retValue.Add(new Contenedor("J1021", 12, 46, "SOL"));
+            retValue.Add(new Contenedor("A4501", 24, 65, "IND"));
+            return retValue;
+        }
+    }
+}
